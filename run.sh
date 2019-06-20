02e996012b57a963c3e67cc1b98ce3452188282a
Anh Hoang Nguyen <hoangkianh31592@gmail.com>
2019-06-20 14:45:02 +0700

    Update install script for zsh plugin

--- run.sh
+++ run.sh
@@ -21,7 +21,9 @@ install_log "Git"
 brew install git
 
 install_log "zsh & plugins"
-brew install zsh zsh-autosuggestions zsh-syntax-highlighting
+brew install zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 install_log "oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
