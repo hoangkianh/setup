4c0e7a2757aaa9fce3dd7953eebafaa14aaf72c6
Anh Hoang Nguyen <hoangkianh31592@gmail.com>
2021-04-07 10:29:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -101,6 +101,9 @@ brew install tmux
 install_log "NeoVim"
 brew install neovim
 
+install_log "ripgrep"
+brew install ripgrep
+
 install_log "Show all hidden files"
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder /System/Library/CoreServices/Finder.app
